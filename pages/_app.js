69a47c3c8dcd7a58946f6f620012e4b26d1e48f2
tedd <15193400+epaumier@users.noreply.github.com>
2026-01-13 11:06:09 +0100

    minimal js  initialization

--- pages/_app.js
+++ pages/_app.js
@@ -1,46 +1,11 @@
-import React from 'react';
-import '../global.css';
-import NavigationMenu from '../components/NavigationMenu';
-import UserTypeSelector from '../components/UserTypeSelector';
-import { AuthProvider, useAuth } from '../contexts/AuthContext';
+// import '../global.css';
+import { AuthProvider } from '../contexts/AuthContext';
 
-function AppContent({ Component, pageProps }) {
-  const { isLoading, logout, userInfo } = useAuth();
-
-  if (isLoading) {
-    return React.createElement(
-      'div',
-      { className: 'flex items-center justify-center min-h-screen' },
-      React.createElement(
-        'div',
-        { className: 'text-center' },
-        React.createElement('p', { className: 'text-lg' }, 'Chargement...')
-      )
-    );
-  }
-
-  return React.createElement(
-    'div',
-    { className: "flex flex-col gap-6 mx-auto my-12 w-[1208px] min-h-[842px] font-['Open_Sans']" },
-    React.createElement(UserTypeSelector, null),
-    React.createElement(
-      'div',
-      { className: 'flex gap-6' },
-      React.createElement(NavigationMenu, { onLogout: logout, userType: userInfo?.userType }),
-      React.createElement(
-        'div',
-        { className: 'flex-1' },
-        React.createElement(Component, pageProps)
-      )
-    )
-  );
-}
-
-function App(props) {
-  return React.createElement(
-    AuthProvider,
-    null,
-    React.createElement(AppContent, props)
+function App({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
   );
 }
 
